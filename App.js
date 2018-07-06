3bf4a06701e552c937c8fcfedeb9b83a7b926d00
Nikita Skrypchenko <frikadelka456@gmail.com>
2018-07-06 19:50:23 +0300

    BookList in strange way

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Weather from './src/Weather';
 import Touchable from './src/Touchable'
+import BookList from './src/BookList';
+
 export default class App extends React.Component {
     render(){
         return(
-            <Touchable/>
+            <BookList/>
         )
     }
 }
